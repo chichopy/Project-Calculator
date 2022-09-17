7eb6ede69b602cdce8840695ffd4e9d965c6bba3
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-17 19:51:00 -0400

    Finish connecting front-end to back-end
    At this point the calculator works pretty well. It still requires validation from the user, and also more features, like on/off, clear window, percentage, etc

--- app.js
+++ app.js
@@ -1,36 +1,65 @@
-//"use strict";
+// add prender y apagar, limpiar y %
+"use strict";
 function add (a, b) {
+    console.log(`In add a = ${a} y b= ${b}`);
     return a + b;
 }
 
 function subtract (a , b) {
+    console.log(`In sub a = ${a} y b= ${b}`);
     return a - b;
 }
 
 function multiply (a, b) {
+    console.log(`In mul a = ${a} y b= ${b}`);
     return a * b;
 }
 
 function divide(a, b) {
+    console.log(`In div a = ${a} y b= ${b}`);
     return a / b
 }
 
-console.log(add(5,6));
-console.log(subtract(5,6));
-console.log(multiply(5,6));
-console.log(divide(5,6));
-
 const buttons = document.querySelectorAll('#button');
-console.log(buttons);
 let string= '';
 buttons.forEach(button => {
     button.addEventListener('click', () => {
     string += button.innerHTML;
-    console.log(string);
     })
 });
 
 const equal = document.getElementById('equal');
 equal.addEventListener('click', () =>{
-    console.log(equal.innerHTML);
+    let [a, b, operations] = splitValues();
+
+    if(operations === '+'){
+        console.log(add(a,b));
+    } else if(operations === '-') {
+        console.log(subtract(a,b));
+    } else if(operations === '*') {
+        console.log(multiply(a,b));
+    } else {
+        console.log(divide(a,b));
+    }
 })
+
+function splitValues(){
+    let operations ='';
+    //validate input
+    // let signAtStart = '';
+    // if (string[0] === '-') {
+    //     signAtStart = string[0]
+    // } else if (signAtStart === '*' || signAtStart === '/') {
+    //     return 'ERROR';
+    // }
+
+    for ( let i = 0; i < string.length; i++) {
+        if ((string[i].codePointAt(0) < 48 || string[i].codePointAt(0) > 57) && string[i].codePointAt(0) !== 46) {
+            operations = string[i];
+        }
+    }
+
+    let [a , b] = string.split(operations);
+    string = '';
+    return [Number(a), Number(b), operations];
+}
