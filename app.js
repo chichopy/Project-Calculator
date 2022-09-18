308e17c0ee511d8b3cdedbe62aed2e45a0e77884
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-18 11:02:31 -0400

    Fix bug that made numbers appear in display even when calculator was off

--- app.js
+++ app.js
@@ -1,11 +1,85 @@
 // add prender y apagar, limpiar y %
 "use strict";
 let string= 0;
+let on = false;
 const showOperation = document.getElementById('showOperation');
 
 turnOnOff();
 getInput();
 equalOperation();
+
+
+function getInput (){
+    const buttons = document.querySelectorAll('#button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (on) {
+                if (string === 0){
+                    string = '';
+                }
+                string += button.innerHTML;
+                showOperation.textContent = string; 
+            }
+        })
+    });
+}
+
+
+function equalOperation() {
+    const equal = document.getElementById('equal');
+    equal.addEventListener('click', () =>{
+        if (on) {
+            let [a, b, operations] = splitValues();
+    
+            if(operations === '+'){
+                showOperation.textContent = add(a,b);
+            } else if(operations === '-') {
+                showOperation.textContent = subtract(a,b);
+            } else if(operations === '*') {
+                showOperation.textContent =multiply(a,b);
+            } else {
+                showOperation.textContent =divide(a,b);
+            }
+            string = 0;
+        }
+    })
+}
+
+
+function splitValues(){
+    if (on) {
+        let operations ='';
+        for ( let i = 0; i < string.length; i++) {
+            if ((string[i].codePointAt(0) < 48 || string[i].codePointAt(0) > 57) && string[i].codePointAt(0) !== 46) {
+                operations = string[i];
+            }
+            // If user only provided one number operations = +
+            if (operations === '') {
+                operations = '+';
+            }
+        }
+    
+        // If user only provided one number b = +
+        let [a , b = 0] = string.split(operations);
+        string = '';
+        return [Number(a), Number(b), operations]; 
+    }
+}
+
+function turnOnOff(){
+    const onButton = document.getElementById('onButton');
+    onButton.addEventListener('click', () => {
+        if (on === false) {
+            console.log('hola')
+            showOperation.textContent = 0; 
+            on = true; 
+        } else {
+            console.log('chau')
+            showOperation.textContent = '';
+            on = false; 
+        }
+    })
+}
 
 
 
@@ -29,78 +103,6 @@ function divide(a, b) {
     return `ANS = ${a / b}`;
 }
 
-
-equalOperation();
-turnOnOff();
-
-
-function getInput (){//showOperation) {
-    const buttons = document.querySelectorAll('#button');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            if (string === 0){
-                string = '';
-            }
-            string += button.innerHTML;
-            showOperation.textContent = string;
-            //console.log('hola');
-        })
-    });
-}
-
-
-function equalOperation() {
-    const equal = document.getElementById('equal');
-    equal.addEventListener('click', () =>{
-        let [a, b, operations] = splitValues();
-    
-        if(operations === '+'){
-            showOperation.textContent = add(a,b);
-        } else if(operations === '-') {
-            showOperation.textContent = subtract(a,b);
-        } else if(operations === '*') {
-            showOperation.textContent =multiply(a,b);
-        } else {
-            showOperation.textContent =divide(a,b);
-        }
-        string = 0;
-    })
-}
-
-
-function splitValues(){
-    let operations ='';
-    for ( let i = 0; i < string.length; i++) {
-        if ((string[i].codePointAt(0) < 48 || string[i].codePointAt(0) > 57) && string[i].codePointAt(0) !== 46) {
-            operations = string[i];
-        }
-        // If user only provided one number operations = +
-        if (operations === '') {
-            operations = '+';
-        }
-    }
-
-    // If user only provided one number b = +
-    let [a , b = 0] = string.split(operations);
-    string = '';
-    return [Number(a), Number(b), operations];
-}
-
-function turnOnOff(){
-    const ON = document.getElementById('ON');
-    let flag = 0;
-
-    ON.addEventListener('click', (event) => {
-        if (flag === 0) {
-            showOperation.textContent = string; 
-            flag = 1; 
-        } else {
-            showOperation.textContent = ''; 
-            flag = 0; 
-        }
-    })
-}
 
 
 
