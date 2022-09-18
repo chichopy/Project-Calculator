f24818f5fdc8eb21a9dd74dd4ea8dc7c005a7d89
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-18 18:24:21 -0400

    Fix bug
    Now if the user does not enter a number, the calculator still works

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// add prender y apagar, limpiar y %
 "use strict";
 let string= 0;
 let previousAnswer = '0';
+let numberOfOperations = 0;
 let on = false;
 const showOperation = document.getElementById('showOperation');
 
@@ -11,6 +11,7 @@ equalOperation();
 clear();
 deleteNumber();
 ans();
+operate();
 
 
 function getInput (){
@@ -28,27 +29,15 @@ function getInput (){
     });
 }
 
-
 function equalOperation() {
     const equal = document.getElementById('equal');
     equal.addEventListener('click', () =>{
         if (on) {
             let [a, b, operations] = splitValues();
-    
-            if(operations === '+'){
-                showOperation.textContent = add(a,b);
-            } else if(operations === '-') {
-                showOperation.textContent = subtract(a,b);
-            } else if(operations === '*') {
-                showOperation.textContent =multiply(a,b);
-            } else {
-                showOperation.textContent =divide(a,b);
-            }
-            string = 0;
+            operate(a, b, operations);
         }
     })
 }
-
 
 function splitValues(){
     if (on) {
@@ -56,15 +45,19 @@ function splitValues(){
         for ( let i = 0; i < string.length; i++) {
             if ((string[i].codePointAt(0) < 48 || string[i].codePointAt(0) > 57) && string[i].codePointAt(0) !== 46) {
                 operations = string[i];
-            }
-            // If user only provided one number operations = +
-            if (operations === '') {
-                operations = '+';
+                numberOfOperations += 1;
             }
         }
-    
-        // If user only provided one number b = +
-        let [a , b = 0] = string.split(operations);
+        // If user only provided one number operations = +
+        if (operations === '') {
+            operations = '+';
+            numberOfOperations = 1;
+        }
+        if (string === 0){
+            string = '';
+        }
+
+        let [a = 0 , b = 0] = string.split(operations);
         string = '';
         return [Number(a), Number(b), operations]; 
     }
@@ -74,11 +67,9 @@ function turnOnOff(){
     const onButton = document.getElementById('onButton');
     onButton.addEventListener('click', () => {
         if (on === false) {
-            //console.log('hola')
             showOperation.textContent = 0; 
             on = true; 
         } else {
-            //console.log('chau')
             string = '';
             showOperation.textContent = '';
             on = false; 
@@ -98,7 +89,6 @@ function deleteNumber(){
     const deleteButton = document.getElementById('delete');
     deleteButton.addEventListener('click', () => {
         if (string.length != 0 && string !== 0){
-            //console.log(string);
             string = string.slice(0, string.length-1);
         }
         showOperation.textContent = string;
@@ -119,65 +109,48 @@ function ans(){
 
 
 
+function operate(a, b, operations){
 
+    if (numberOfOperations === 1){
+        if(operations === '+'){
+            showOperation.textContent = add(a,b);
+        } else if(operations === '-') {
+            showOperation.textContent = subtract(a,b);
+        } else if(operations === '*') {
+            showOperation.textContent =multiply(a,b);
+        } else {
+            showOperation.textContent =divide(a,b);
+        }
+        string = 0;
+        numberOfOperations = 0;
+    } else if (numberOfOperations > 1) {
+        showOperation.textContent = 'Solve only one operation at a time.\nFor example a + b. NOT a + b +c';
+        string = 0;
+        numberOfOperations = 0;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function add (a, b) {
-    //console.log(`In add a = ${a} y b= ${b}`);
-    previousAnswer = (a + b).toString();
-    //console.log(previousAnswer);
-    return `ANS = ${a + b}`;
+    function add (a, b) {
+        //console.log(`In add a = ${a} y b= ${b}`);
+        previousAnswer = (a + b).toString();
+        //console.log(previousAnswer);
+        return `ANS = ${a + b}`;
+    }
+    
+    function subtract (a , b) {
+        //console.log(`In sub a = ${a} y b= ${b}`);
+        previousAnswer = (a - b).toString();
+        return `ANS = ${a - b}`;
+    }
+    
+    function multiply (a, b) {
+        //console.log(`In mul a = ${a} y b= ${b}`);
+        previousAnswer = (a * b).toString();
+        return `ANS = ${a * b}`;
+    }
+    
+    function divide(a, b) {
+        //console.log(`In div a = ${a} y b= ${b}`);
+        previousAnswer = (a / b).toString();
+        return `ANS = ${a / b}`;
+    }
 }
-
-function subtract (a , b) {
-    //console.log(`In sub a = ${a} y b= ${b}`);
-    previousAnswer = (a - b).toString();
-    return `ANS = ${a - b}`;
-}
-
-function multiply (a, b) {
-    //console.log(`In mul a = ${a} y b= ${b}`);
-    previousAnswer = (a * b).toString();
-    return `ANS = ${a * b}`;
-}
-
-function divide(a, b) {
-    //console.log(`In div a = ${a} y b= ${b}`);
-    previousAnswer = (a / b).toString();
-    return `ANS = ${a / b}`;
-}
-
-
-
-
-
-// function validate(){
-//     let signAtStart = '';
-//     if (string[0] === '-') {
-//         signAtStart = string[0]
-//     } else if (signAtStart === '*' || signAtStart === '/') {
-//         return 'ERROR';
-//     }
-// }
