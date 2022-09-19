9a32dea402598d3d9ef7c1df17a2d259629439d0
Guillermo Sanchez <gsanz1997@gmail.com>
2022-09-19 08:42:18 -0400

    Fix problems of using float numbers
    Now the user can add, subtract, multiply and divide numbers with a precision of 3 in float. Next step create a function to valide user input

--- app.js
+++ app.js
@@ -8,13 +8,30 @@ const showOperation = document.getElementById('showOperation');
 turnOnOff();
 getInput();
 equalOperation();
+operate();
+ans();
 clear();
 deleteNumber();
-ans();
-operate();
 
 
-function getInput (){
+function turnOnOff() {
+    const onButton = document.getElementById('onButton');
+    showOperation.style['align-items'] = 'flex-end';
+    onButton.addEventListener('click', () => {
+        showOperation.style['align-items'] = 'flex-end';
+        if (on === false) {
+            showOperation.textContent = 0; 
+            on = true; 
+        } else {
+            string = '';
+            showOperation.textContent = '';
+            on = false; 
+        }
+    })
+}
+
+
+function getInput () {
     const buttons = document.querySelectorAll('#button');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
@@ -23,23 +40,27 @@ function getInput (){
                     string = '';
                 }
                 string += button.innerHTML;
+                showOperation.style['align-items'] = 'flex-end';
                 showOperation.textContent = string; 
             }
         })
     });
 }
 
+
 function equalOperation() {
     const equal = document.getElementById('equal');
     equal.addEventListener('click', () =>{
         if (on) {
             let [a, b, operations] = splitValues();
+            showOperation.style['align-items'] = 'flex-end';
             operate(a, b, operations);
         }
     })
 }
 
-function splitValues(){
+
+function splitValues() {
     if (on) {
         let operations ='';
         for ( let i = 0; i < string.length; i++) {
@@ -63,54 +84,8 @@ function splitValues(){
     }
 }
 
-function turnOnOff(){
-    const onButton = document.getElementById('onButton');
-    onButton.addEventListener('click', () => {
-        if (on === false) {
-            showOperation.textContent = 0; 
-            on = true; 
-        } else {
-            string = '';
-            showOperation.textContent = '';
-            on = false; 
-        }
-    })
-}
 
-function clear(){
-    const clear = document.getElementById('clear');
-    clear.addEventListener('click', () => {
-        string = 0;
-        showOperation.textContent = string;
-    })
-}
-
-function deleteNumber(){
-    const deleteButton = document.getElementById('delete');
-    deleteButton.addEventListener('click', () => {
-        if (string.length != 0 && string !== 0){
-            string = string.slice(0, string.length-1);
-        }
-        showOperation.textContent = string;
-    })
-}
-
-function ans(){
-    const ansButton = document.getElementById('ans');
-    ansButton.addEventListener('click', () => {
-        string = previousAnswer;
-        showOperation.textContent = string;
-    })
-}
-
-
-
-
-
-
-
-function operate(a, b, operations){
-
+function operate(a, b, operations) {
     if (numberOfOperations === 1){
         if(operations === '+'){
             showOperation.textContent = add(a,b);
@@ -124,33 +99,95 @@ function operate(a, b, operations){
         string = 0;
         numberOfOperations = 0;
     } else if (numberOfOperations > 1) {
-        showOperation.textContent = 'Solve only one operation at a time.\nFor example a + b. NOT a + b +c';
+        showOperation.style['align-items'] = 'baseline';
+        let p = document.createElement('p');
+        let p0 = document.createElement('p');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        p.textContent = 'SYNTAX ERROR  a + b +....='
+        p0.textContent = 'Solve only one operation at a time.'
+        p1.textContent = 'For example a + b =.';
+        p2.textContent = 'For multiple operations use ANS';
+        showOperation.removeChild(showOperation.firstChild);
+        showOperation.appendChild(p);
+        showOperation.appendChild(p0);
+        showOperation.appendChild(p1);
+        showOperation.appendChild(p2);
         string = 0;
         numberOfOperations = 0;
     }
 
     function add (a, b) {
-        //console.log(`In add a = ${a} y b= ${b}`);
-        previousAnswer = (a + b).toString();
-        //console.log(previousAnswer);
-        return `ANS = ${a + b}`;
+        if (parseFloat(a) === parseInt(a) && parseFloat(b) === parseInt(b)){
+            previousAnswer = (a + b).toString();
+            return `ANS = ${previousAnswer}`;
+        } else {
+            previousAnswer = (((a * 10) + (b * 10)) / 10).toFixed(3);
+            return `ANS = ${previousAnswer}`;
+        }
     }
     
     function subtract (a , b) {
-        //console.log(`In sub a = ${a} y b= ${b}`);
-        previousAnswer = (a - b).toString();
-        return `ANS = ${a - b}`;
+        if (parseFloat(a) === parseInt(a) && parseFloat(b) === parseInt(b)){
+            previousAnswer = (a - b).toString();
+            return `ANS = ${previousAnswer}`;
+        } else {
+            previousAnswer = (((a * 10) - (b * 10)) / 10).toFixed(3);
+            return `ANS = ${previousAnswer}`;
+        }
     }
     
     function multiply (a, b) {
-        //console.log(`In mul a = ${a} y b= ${b}`);
-        previousAnswer = (a * b).toString();
-        return `ANS = ${a * b}`;
+        if (parseFloat(a) === parseInt(a) && parseFloat(b) === parseInt(b)){
+            previousAnswer = (a * b).toString();
+            return `ANS = ${previousAnswer}`;
+        } else {
+            previousAnswer = (((a * 10) * (b * 10)) / 10).toFixed(3);
+            return `ANS = ${previousAnswer}`;
+        }
     }
     
     function divide(a, b) {
-        //console.log(`In div a = ${a} y b= ${b}`);
-        previousAnswer = (a / b).toString();
-        return `ANS = ${a / b}`;
+        if (parseFloat(a) === parseInt(a) && parseFloat(b) === parseInt(b) && b !== 0){
+            previousAnswer = (a / b).toFixed(3).toString();
+            return `ANS = ${previousAnswer}`;
+        } if (b ===0){
+            return `SYNTAX ERROR`;
+        } else {
+            previousAnswer = (((a * 10) / (b * 10)) / 10).toFixed(3);
+            return `ANS = ${previousAnswer}`;
+        }
     }
+}
+
+
+function ans() {
+    const ansButton = document.getElementById('ans');
+    ansButton.addEventListener('click', () => {
+        string = previousAnswer;
+        showOperation.style['align-items'] = 'flex-end';
+        showOperation.textContent = string;
+    })
+}
+
+
+function clear() {
+    const clear = document.getElementById('clear');
+    clear.addEventListener('click', () => {
+        string = 0;
+        showOperation.style['align-items'] = 'flex-end';
+        showOperation.textContent = string;
+    })
+}
+
+
+function deleteNumber() {
+    const deleteButton = document.getElementById('delete');
+    deleteButton.addEventListener('click', () => {
+        if (string.length != 0 && string !== 0){
+            string = string.slice(0, string.length-1);
+        }
+        showOperation.style['align-items'] = 'flex-end';
+        showOperation.textContent = string;
+    })
 }
